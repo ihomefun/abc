084797e82c1195b7e1359fbaf60c4c0925266f67
Scott Brooks <scottb@tsbrooks.com>
2013-05-09 05:53:49 -0400

    Added one Model and it compiles

---
+++ Abc.WebRole/Models/InfoRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abc.WebRole.Models
+{
+    public class InfoRequest
+    {
+        public string Email { get; set; }
+        public DateTime DateAddedUtc { get; set; }
+    }
+}
